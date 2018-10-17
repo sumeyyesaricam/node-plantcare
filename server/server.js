7d0d8757508cd33bf7b03e4749a13bf97978395d
DESKTOP-JFJTKFD\sumey <sumeyye98762@gmail.com>
2018-10-17 23:32:59 +0300

    insert delete getlist metoları eklendi.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose =require('./db/mongoose');
+const { ObjectID } = require('mongodb');
+var mongoose = require('./db/mongoose');
 var plant = require('./models/plant');
+const port = process.env.PORT || 3000;
 var app = express();
-const port =process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/insertPlant', (req, res) => {
@@ -21,10 +23,70 @@ app.post('/insertPlant', (req, res) => {
         res.send(doc);
         console.log('Eklendi.');
     }, (err) => {
-        console.log('Hatali.'+err);
+        console.log('Hatali.' + err);
         res.status(400).send(err);
     });
 });
+
+app.get('/getPlants', (req, res) => {
+    plant.find().then((plants) => {
+        res.send(plants);
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});
+
+app.get('/getPlant/:id', (req, res) => {
+    var id = req.params.id
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send("geçerli değil");
+    }
+    plant.findById(id).then((plant) => {
+        if (!plant) {
+            return res.status(400).send("not found");
+        }
+        res.send(plant);
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+});
+
+app.delete('/removePlant/:id', (req, res) => {
+    var id = req.params.id
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send("geçerli değil");
+    }
+    plant.findByIdAndRemove(id).then((plant) => {
+        if (!plant) {
+            return res.status(400).send("not found");
+        }
+        res.send(plant);
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+});
+
+app.patch('/editPlant/:id', (req, res) => {
+    var id = req.params.id
+    var body = _.pick(req.body, ['name']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send("geçerli değil");
+    }
+    if (_.isBoolean())
+        plant.findOneAndUpdate(id, {
+            $set: body
+        }, {
+                new: true
+            }).then((plant) => {
+                if (!plant) {
+                    return res.status(400).send("not found");
+                }
+                res.send(plant);
+            }).catch((err) => {
+                res.status(400).send(err);
+            })
+});
+
 app.listen(port, () => {
     console.log('Started on port 3000');
 });
